1c05a2ecd7e03d72585215ff337989f18d63bbd2
PAVITHRA786786 <PAVITHRA124CHENNAVEERA@GMAIL.COM>
2025-05-27 22:04:31 +0530

    updated

---
+++ classwork/day30/mainl.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+
+#include "linkedlist.h"
+
+using namespace std;
+
+int main()
+{
+	//LinkedList l1(10);
+	LinkedList l2;
+	if (l2.delNode(10) != true)
+		cout << "Target not found" << endl;
+	l2.addEnd(10);
+	l2.addEnd(11);
+	l2.addEnd(12);
+
+	l2.display();
+	int target = 11;
+	int val = 50;
+	l2.addAfter(target, val);
+	l2.display();
+
+	if (l2.delNode(10) != true)
+		cout << "Target not found" << endl;
+	else
+		cout << "Target " << 10 << " got delelted" << endl;
+	l2.display();
+
+	return 0;
+}
